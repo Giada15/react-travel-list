ddeb0ba3f93ad8f5bf7ec29ce21b477beb91e35b
Giada15 <tascheriogiada@gmail.com>
2025-05-25 11:26:39 -0400

    feat: create form with options array mapped into select element and onSubmit handler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "Charger", quantity: 1, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
 ];
 
 export default function App() {
@@ -20,9 +20,24 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="add-form">
+    <div className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
+      <form>
+        <select>
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
+        </select>
+        <input type="text" placeholder="Item..."></input>
+        <button>Add</button>
+      </form>
     </div>
   );
 }
@@ -32,7 +47,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
